37a08fe3a79dfee839a55bfa024b055d43338c57
no-choice <no-choice@users.noreply.github.com>
2017-07-31 16:22:44 +0300

    adding jacobi stub

---
+++ smothers/src/main/java/acc/edu/multigrid/amg/smoothers/concrete/JacobiSmoother.java
@@ -0,0 +1,13 @@
+package acc.edu.multigrid.amg.smoothers.concrete;
+
+import acc.edu.multigrid.amg.datatypes.faces.SparseMatrix;
+import acc.edu.multigrid.amg.smoothers.faces.Smoother;
+
+public class JacobiSmoother implements Smoother {
+
+	@Override
+	public double[] smooth(SparseMatrix matrix) {
+		return null;
+	}
+
+}
--- smothers/src/main/java/acc/edu/multigrid/amg/smoothers/faces/Smoother.java
+++ smothers/src/main/java/acc/edu/multigrid/amg/smoothers/faces/Smoother.java
@@ -1,5 +1,8 @@
 package acc.edu.multigrid.amg.smoothers.faces;
 
+import acc.edu.multigrid.amg.datatypes.faces.SparseMatrix;
+
 public interface Smoother {
+	public double[] smooth(SparseMatrix matrix);
 
 }
